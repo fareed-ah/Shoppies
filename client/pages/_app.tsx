5001d2b0112dc16a8daca2ac41607672d4f6b6b6
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2021-04-28 11:44:26 -0400

    Add material ui

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import React from 'react'
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  <React.Fragment>
+    <Head>
+      <title>Shoppies</title>
+      <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    </Head>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </React.Fragment>
 }
 
 export default MyApp
