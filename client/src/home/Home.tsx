66994c9d862dfef450a10f21fd85c74b446e40f9
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2021-04-29 15:28:59 -0400

    Fetch and display movies

--- client/src/home/Home.tsx
+++ client/src/home/Home.tsx
@@ -1,18 +1,31 @@
 import { Box, Icon, Typography } from '@material-ui/core';
-import React from 'react'
-import SearchBar from '../searchbar/SearchBar';
+import React, { useState } from 'react'
+import { Row } from '../categories/Row';
+import { SearchBar } from '../searchbar/SearchBar';
 
 interface HomeProps {
 
 }
 
+export type Movie = {
+    Title: string,
+    Year: string,
+    Poster: string,
+    imdbID: string,
+}
+
 export const Home: React.FC<HomeProps> = ({ }) => {
+    const [searchResults, setSearchResults] = useState<Movie[]>([]);
+
     return (
         <Box minHeight="100vh" minWidth="100%">
             <Box justifyContent="center" alignItems="center" display="flex" flexDirection="column" minHeight="50vh" minWidth="100%">
                 <Icon />
-                <Typography variant="h3">Shoppies</Typography>
-                <SearchBar />
+                <Typography variant="h3">The Shoppies</Typography>
+                <SearchBar setSearchResults={setSearchResults} />
+            </Box>
+            <Box justifyContent="center" alignItems="center" display="flex" flexDirection="column" minHeight="50vh" minWidth="100%">
+                <Row movieData={searchResults}></Row>
             </Box>
         </Box>
     );
--- client/src/searchbar/SearchBar.tsx
+++ client/src/searchbar/SearchBar.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import { Clear } from '@material-ui/icons';
+import axios from 'axios';
+import { Movie } from '../home/Home';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -29,15 +31,37 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
-export default function SearchBar() {
+interface SearchBarProps {
+    setSearchResults: React.Dispatch<React.SetStateAction<Movie[]>>
+}
+
+
+
+export const SearchBar: React.FC<SearchBarProps> = ({ setSearchResults }: SearchBarProps) => {
     const classes = useStyles();
+    const [title, setTitle] = useState('');
+
+    const handleSubmit = (evt: { preventDefault: () => void; }) => {
+        evt.preventDefault();
+        axios.post(`https://www.omdbapi.com/?apikey=dd016357&s=${title}`)
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+                const movies = res.data.Search;
+                setSearchResults(movies);
+            })
+    };
 
     return (
-        <Paper component="form" className={classes.root}>
+        <Paper component="form" onSubmit={handleSubmit} className={classes.root}>
             <IconButton type="submit" className={classes.iconButton} aria-label="search">
                 <SearchIcon />
             </IconButton>
             <InputBase
+                onChange={(e) =>
+                    setTitle(e.currentTarget.value)
+                }
+                value={title}
                 className={classes.input}
                 placeholder="Search for a movie"
             />
