716a7a87724fcc9f3a0b64e3f66f2bd4117aea11
Fareed Ahmad <fareedahmad@cmail.carleton.ca>
2021-04-29 19:56:20 -0400

    Add nominations tab

--- client/src/home/Home.tsx
+++ client/src/home/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Icon, Typography } from '@material-ui/core';
+import { Box, Button, Card, Icon, Typography } from '@material-ui/core';
 import React, { useState } from 'react'
 import { Row } from '../categories/Row';
 import { SearchBar } from '../searchbar/SearchBar';
@@ -16,16 +16,31 @@ export type Movie = {
 
 export const Home: React.FC<HomeProps> = ({ }) => {
     const [searchResults, setSearchResults] = useState<Movie[]>([]);
+    const [nominations, setNominations] = useState<Movie[]>([]);
+    const [showResults, setShowResults] = useState(true);
+
+    const addNomination = (movie: Movie) => {
+        setNominations([...nominations, movie])
+    }
+    const removeNomination = (movie: Movie) => {
+        setNominations(nominations.filter(nominatedMovie => nominatedMovie.imdbID != movie.imdbID))
+    }
+
+    const isNominated = (movie: Movie): boolean => {
+        return nominations.some(nominatedMovie => nominatedMovie.imdbID === movie.imdbID)
+    }
 
     return (
-        <Box minHeight="100vh" minWidth="100%">
-            <Box justifyContent="center" alignItems="center" display="flex" flexDirection="column" minHeight="50vh" minWidth="100%">
+        <Box minHeight="100vh" minWidth="100%" display="flex" flexDirection="column" padding={5}>
+            <Box flex="1" flexShrink={0} justifyContent="center" alignItems="center" display="flex" flexDirection="column">
                 <Icon />
                 <Typography variant="h3">The Shoppies</Typography>
                 <SearchBar setSearchResults={setSearchResults} />
             </Box>
-            <Box justifyContent="center" alignItems="center" display="flex" flexDirection="column" minHeight="50vh" minWidth="100%">
-                <Row movieData={searchResults}></Row>
+            <Box flex="3">
+                <Button variant="contained" onClick={() => setShowResults(true)}>Search Results</Button>
+                <Button variant="contained" onClick={() => setShowResults(false)}>Nominations</Button>
+                <Row isNominated={isNominated} removeNomination={removeNomination} isShowingResults={showResults} movieData={showResults ? searchResults : nominations} addNomination={addNomination}></Row>
             </Box>
         </Box>
     );
